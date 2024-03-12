6e2d803733b0d3f34d854e66a039389c6bca450d
ngadzheva <nmgadzheva@gmail.com>
2024-03-12 18:56:40 +0200

    Add JS examples

---
+++ js basics/index.js
@@ -0,0 +1,42 @@
+name = "Nevena";
+
+console.log(name);
+
+// var name;
+// var lastName;
+
+function greeting(age) {
+    // var asdf;
+    lastName = "Gadzheva";
+    
+    if (age > 20) {
+        let asdf = 'fkdflsf'
+        const MAX_NUM = 6;
+    }
+    var lastName;
+    console.log(`Hello, ${name} ${lastName}, ${asdf}`);
+}
+
+// var lastName;
+// greeting(22);
+// console.log(lastName)
+
+console.log('1' !== 1)
+console.log(+'1.05' + 1)
+console.log('1' - 1)
+
+function add(a, b = 0, c = 0) {
+    console.log(a + b + c)
+}
+
+var sum = add;
+
+sum(1, 5, 5);
+sum(4, 6)
+sum(8)
+sum()
+sum(8, 7, 9, 5)
+
+var add = 5 + 2;
+console.log(add)
+// add(5, 8, 9);
